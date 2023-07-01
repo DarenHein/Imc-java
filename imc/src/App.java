6d6d94ba15cbbce16af73e08fd60cb29ce36f873
Luis Manjarrez carbajal <darenhein@Hotmail.com>
2023-07-01 01:38:07 -0600

    v2

---
+++ imc/src/App.java
@@ -0,0 +1,7 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        ventana obj = new ventana(); 
+        obj.setVisible(true);
+        System.out.println("Inicia el programa");
+    }
+}
