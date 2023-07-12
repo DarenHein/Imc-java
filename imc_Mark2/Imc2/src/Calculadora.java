1b8f93af5085f696fa35bff41f7e5e050cbf0f95
DarenHein <darenhein@hotmail.com>
2023-07-11 22:53:23 -0600

    v2

--- imc_Mark2/Imc2/src/Calculadora.java
+++ imc_Mark2/Imc2/src/Calculadora.java
@@ -2,7 +2,6 @@ import java.awt.Color;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
 import javax.swing.*;
 
 public class Calculadora extends JFrame {
@@ -20,6 +19,7 @@ public class Calculadora extends JFrame {
     JTextField campo_imc; 
     JButton boton_regresar; 
     JButton boton_calcular; 
+    JButton boton_registro; 
     String nombre_string; 
     String peso_string; 
     String altura_string; 
@@ -116,14 +116,32 @@ public class Calculadora extends JFrame {
         });
         panel.add(boton_regresar); 
 
+        boton_registro = new JButton("Registrar"); 
+        boton_registro.setBounds(400, 310, 100, 30);
+        boton_registro.setBackground(Color.orange);
+        boton_registro.setForeground(Color.white);
+        boton_registro.setVisible(false);
+        boton_registro.addActionListener(new ActionListener(){
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+               // mandara ala ventana de registro con constructor 
+            }
+
+        });
+        panel.add(boton_registro); 
+
         boton_calcular = new JButton("Calcular");
         boton_calcular.setBounds(250,310,100,30); 
         boton_calcular.setBackground(Color.blue);
         boton_calcular.setForeground(Color.white);
+        boton_calcular.setVisible(true); 
         boton_calcular.addActionListener(new ActionListener() {
 
             @Override
             public void actionPerformed(ActionEvent e) {
+                setLocationRelativeTo(null);
+                boton_registro.setVisible(true);
                 nombre_string = campo_nombre.getText(); 
                try {
                 nombre_string = campo_nombre.getText(); 
@@ -132,6 +150,7 @@ public class Calculadora extends JFrame {
                 altura = Float.parseFloat(altura_string); 
                 peso = Float.parseFloat(peso_string); 
                 if (nombre_string.isEmpty() || altura_string.isEmpty() || peso_string.isEmpty()){
+                     boton_calcular.setVisible(false); 
                     JOptionPane.showMessageDialog(null,"Campos vacios");
                 }
                 else if (altura < 1 || altura > 2  ||altura == 0){
@@ -151,34 +170,66 @@ public class Calculadora extends JFrame {
                     String imc2 = String.valueOf(imc); 
                     etiqueta_resultado.setText(imc2);
                     if (imc <= 18.5){
+                        setLocationRelativeTo(null);
+                        campo_altura.setEditable(false);
+                        campo_peso.setEditable(false);
+                        campo_nombre.setEditable(false);
                         etiqueta_resultado.setForeground(Color.blue);
                         titulo_imc.setForeground(Color.blue);
                         barra.setBackground(Color.blue);
+                        JOptionPane.showMessageDialog(null,"Bajo Peso:\tDieta Hipercalrica \n Come con m\u00E1s frecuencia. Empieza poco a poco a hacer de 5 a 6 comidas m\u00E1s peque\u00F1as durante el d\u00EDa. \nAgrega aderezos. Agrega extras a tus platos para obtener mas calorias, como queso en los guisos o mantequilla \n Prueba licuados y batidos de frutas. Evita las bebidas con pocos nutrientes o calorias, como los refrescos dieteticos. ");
+                               
+                        
                     }
                     else if(imc > 18.5 && imc <= 24.9 ){
+                        setLocationRelativeTo(null);
+                        campo_altura.setEditable(false);
+                        campo_peso.setEditable(false);
+                        campo_nombre.setEditable(false);
                         etiqueta_resultado.setForeground(Color.BLUE);
                         titulo_imc.setForeground(Color.BLUE);
                         barra.setBackground(Color.BLUE);
+                        JOptionPane.showMessageDialog(null,"Peso Normal: Dieta Normocalojica \n Importancia de las frutas. las verduras, los cereales integrales, la leche y los productos l\u00E1cteos sin grasa o bajos en grasa \n Incluye alimentos con prote\u00EDnas. como mariscos, carnes magras, huevos, legumbres (frijoles y guisantes), productos de soya, nueces y semillas \n Consume pocas grasas saturadas. grasas trans, colesterol, sal (sodio) y az\u00FAcares agregados"); 
                     }
                     else if (imc >= 25 && imc <= 29.9 ){
+                        setLocationRelativeTo(null);
+                        campo_altura.setEditable(false);
+                        campo_peso.setEditable(false);
+                        campo_nombre.setEditable(false);
                         etiqueta_resultado.setForeground(Color.green);
                         titulo_imc.setForeground(Color.green);
                         barra.setBackground(Color.green);
+                        JOptionPane.showMessageDialog(null, "Sobre Peso: Dieta Hipocalorica \n Variedad de verduras y frutas. y granos integrales, como arroz integral, avena y pan integral.\r\n productos lacteos. sin grasa o con bajo contenido de grasa, como leche, yogur y queso, y productos similares, como las bebidas de soya \n Ejercicio. Regula la actividad física al menos 5 días a la semana para perder grasa.");
                     }
                     else if (imc >= 30 && imc <= 34.9 ){
+                        setLocationRelativeTo(null);
+                        campo_altura.setEditable(false);
+                        campo_peso.setEditable(false);
+                        campo_nombre.setEditable(false);
                         etiqueta_resultado.setForeground(Color.yellow);
                         titulo_imc.setForeground(Color.yellow);
                         barra.setBackground(Color.yellow);
+                        JOptionPane.showMessageDialog(null,"Obesidad: Dieta Hipocalrica \n Menos porciones. consumir menos alimentos y bebidas que contienen carbohidratos refinados, azucares añadidos y sal (sodio)\r\n Tambien. controlar el tamaño de las porciones por unas m\u00E1s peque\u00F1as durante el d\u00EDa.\r\n Limite. limitar los alimentos con grasas saturadas y grasas trans, como las que se encuentran en los postres y las comidas fritas");
                     }
                     else if (imc >= 35 && imc <= 39.9){
+                        setLocationRelativeTo(null);
+                        campo_altura.setEditable(false);
+                        campo_peso.setEditable(false);
+                        campo_nombre.setEditable(false);
                         etiqueta_resultado.setForeground(Color.ORANGE);
                         titulo_imc.setForeground(Color.ORANGE);
                         barra.setBackground(Color.ORANGE);
+                        JOptionPane.showMessageDialog(null,"Obesidad Extrema: Dieta Hipocalorica\r\nComidas no saludables. Evitar ingerir alimentos pocos saludable que incluya muchas calorias (tanto saturadas como solidas , como los refrescos).\r\n Limite. limitar los alimentos con grasas saturadas y grasas trans, como las que se encuentran en los postres y las comidas fritas\r\n Inactividad. El quemar las calorias que ingieres, el resultado es siempre Mantener un buen peso. \r\n");
                     }
                     else if (imc >= 40 && imc <100){
+                        setLocationRelativeTo(null);
+                        campo_altura.setEditable(false);
+                        campo_peso.setEditable(false);
+                        campo_nombre.setEditable(false);
                         etiqueta_resultado.setForeground(Color.red);
                         titulo_imc.setForeground(Color.red);
                         barra.setBackground(Color.red);
+                        JOptionPane.showMessageDialog(null,"Obesidad Extrema: Dieta Hipocalorica\r\nComidas no saludables. Evitar ingerir alimentos pocos saludable que incluya muchas calorias (tanto saturadas como solidas , como los refrescos).\r\n Limite. limitar los alimentos con grasas saturadas y grasas trans, como las que se encuentran en los postres y las comidas fritas\r\n Inactividad. El quemar las calorias que ingieres, el resultado es siempre Mantener un buen peso. \r\n");
                     }
 
 
@@ -194,6 +245,8 @@ public class Calculadora extends JFrame {
             
         });
         panel.add(boton_calcular);  
+
+        
 
 
         barra = new JLabel(); 
