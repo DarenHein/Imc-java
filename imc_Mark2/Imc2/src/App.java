fd53a8d4f7b1049afb9c225332bee0fbe9457c63
DarenHein <darenhein@hotmail.com>
2023-07-15 17:00:54 -0600

    v2

--- imc_Mark2/Imc2/src/App.java
+++ imc_Mark2/Imc2/src/App.java
@@ -1,6 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+       // System.out.println("Hello, World!");
         System.out.println("Arraca el Login");
         Login obj = new Login(); 
         obj.setVisible(true); 
